5d4807cd5f954125787cb43aa0fd5b2cbfab6324
attila.boncser <bon.ati1995@gmail.com>
2021-10-22 12:39:32 +0200

    Add openfeign support to Appointment service

---
+++ appointment-service/appointment-api/src/main/java/hu/bme/aut/gassys/appointment/AppointmentServiceIF.java
@@ -0,0 +1,17 @@
+package hu.bme.aut.gassys.appointment;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "appointment-service-v1",
+        url = "$services.appointment-service-url:localhost:8083/appointment"
+)
+public interface AppointmentServiceIF {
+
+    @PostMapping()
+    ResponseEntity<AppointmentDTO> CreateAppointment(@RequestBody AppointmentCreationDTO dto);
+}
