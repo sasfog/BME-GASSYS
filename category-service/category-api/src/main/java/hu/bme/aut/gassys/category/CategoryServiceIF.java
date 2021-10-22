a611304af12d9d0893019b17ed3753090d1b5243
attila.boncser <bon.ati1995@gmail.com>
2021-10-22 12:35:23 +0200

    Add openfeign support to Category service

---
+++ category-service/category-api/src/main/java/hu/bme/aut/gassys/category/CategoryServiceIF.java
@@ -0,0 +1,16 @@
+package hu.bme.aut.gassys.category;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(
+        name = "category-service-v1",
+        url = "$services.category-service-url:localhost:8081/category"
+)
+public interface CategoryServiceIF {
+
+    @GetMapping("/{id}")
+    ResponseEntity<CategoryDTO> findOneCategory(@PathVariable Integer id);
+}
