df642980a37e044154cbc2997ed911beda6b2c2d
attila.boncser <bon.ati1995@gmail.com>
2021-12-10 03:48:06 +0100

    Fix Settings page missing new password input.

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,15 +1,22 @@
 <template>
   <div class="w-100 vh-100 d-flex justify-content-center align-items-center">
     <form class="w-25 border border-info border-2 rounded">
-      <div class="d-flex flex-column justify-content-center align-items-center bg-light">
-      <h1>{{ userNameText }}</h1>
-      <h3>{{ userEmailText }}</h3>
-      <label for="password">Password</label>
-      <input v-model="password" placeholder="Password.." name="password"/>
-      <label for ="confirmPassword" >Confirm Password</label>
-      <input v-model="confirmPassword" placeholder="Password.." name="confirmPassword" />
-      <button type="button" class="btn-primary my-3">Save</button>
-    </div>
+      <div
+        class="d-flex flex-column justify-content-center align-items-center bg-light"
+      >
+        <h1>{{ userNameText }}</h1>
+        <h3>{{ userEmailText }}</h3>
+        <label for="password">Password</label>
+        <input v-model="password" name="password" />
+        <label for="newPassword">New Password</label>
+        <input v-model="newPassword" name="newPassword" />
+        <label for="confirmNewPassword">Confirm Password</label>
+        <input
+          v-model="confirmNewPassword"
+          name="confirmNewPassword"
+        />
+        <button type="button" class="btn-primary my-3">Save</button>
+      </div>
     </form>
   </div>
 </template>
@@ -22,16 +29,17 @@ export default {
   data() {
     return {
       password: "",
-      confirmPassword: "",
+      newPassword: "",
+      confirmNewPassword: "",
     };
   },
   computed: {
     userNameText() {
-      return this.$store.getters.StateUser
+      return this.$store.getters.StateUser;
     },
-    userEmailText(){
-      return this.$store.getters.StateEmail
-    }
-  }
+    userEmailText() {
+      return this.$store.getters.StateEmail;
+    },
+  },
 };
 </script>
